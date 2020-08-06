1bedb4597c591c5c582f8221be9578590f1014c7
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2020-08-06 16:15:17 +0300

    data base conf

--- config/database.php
+++ config/database.php
@@ -37,11 +37,10 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            // 'url' => env('DATABASE_URL'),
-            // 'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'database' => ':memory',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            // 'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
