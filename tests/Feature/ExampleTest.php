90c5426eef6fd0b72b80a16476957453292fcac7
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2020-08-06 14:58:44 +0300

    make test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/article/');
 
         $response->assertStatus(200);
     }
